51f86fb94065a251ff0d6e86db9ea05ee471fffe
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-04-06 16:45:07 +0200

    Add migration between user and words (AddUserToWords)

---
+++ db/migrate/20220406141647_adduser_to_words.rb
@@ -0,0 +1,5 @@
+class AdduserToWords < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :words, :user, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_04_170602) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_06_141647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,8 +37,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_04_170602) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "language_id"
+    t.bigint "user_id"
     t.index ["language_id"], name: "index_words_on_language_id"
+    t.index ["user_id"], name: "index_words_on_user_id"
   end
 
   add_foreign_key "words", "languages"
+  add_foreign_key "words", "users"
 end
