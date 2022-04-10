be6202c3d60f2e45e6706d7e21faf73db269718a
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-04-10 18:12:38 +0200

    write spec for translation model

--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Translation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:word) }
+    it { is_expected.to belong_to(:translated_word).class_name('Word') }
+  end
 end
